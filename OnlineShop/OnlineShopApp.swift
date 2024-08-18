0c5a2a8d728b1d0b67aaf6fc973fe7d6fb4ac02a
IbragimovAzat <appleses@icloud.com>
2024-08-18 20:30:55 +0500

    Initial Commit

---
+++ OnlineShop/OnlineShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineShopApp.swift
+//  OnlineShop
+//
+//  Created by Азат Ибрагимов on 18.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct OnlineShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
